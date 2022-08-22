f14a883842900d32db5ee18ed10ce342ab1fcaa6
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-08-21 22:43:17 -0300

    feat:starting project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Welcome } from "./pages/Welcome";
+import { GlobalStyles } from "./styles/globalStyles";
+
+function App() {
+  return (
+    <>
+      <Welcome />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
